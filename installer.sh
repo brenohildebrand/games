78265444f7deb1502357273f181c894147113293
Breno Hildebrand <hello@brenohildebrand.com>
2025-02-19 16:30:03 -0400

    ok

--- installer.sh
+++ installer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-URL="https://github.com/brenohildebrand/games/raw/refs/heads/main/$(cat).tar.gz"
+URL="https://github.com/brenohildebrand/games/raw/refs/heads/main/$1.tar.gz"
 
 curl -sL "$URL" | tar -xz
